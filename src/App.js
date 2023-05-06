b0657fcae0df5dbae4998b6acf537f74042b4078
Hans Jeremy <Hansjrm@gmail.com>
2023-05-05 17:54:20 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const [holidays, setHolidays] = React.useState([]);
+  const [isDataLoaded, setIsDataLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://date.nager.at/api/v2/publicholidays/2023/US")
+      .then((res) => res.json())
+      .then((json) => {
+        setHolidays(json);
+        setIsDataLoaded(true);
+      });
+  }, []);
+
+  if (!isDataLoaded) {
+    return (
+      <div>
+        <h1> Please chill, data is still loading </h1>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Holidays </h1>
+      {holidays.map((holiday) => (
+        <ul key={holiday.id}>
+          <li>
+            
+            <h2>Holiday: {holiday.localName}</h2>
+            Date: {holiday.date}
+			<br />Other Names: {holiday.name}
+          </li>
+        </ul>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
